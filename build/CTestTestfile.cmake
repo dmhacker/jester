b38c1466730a1a0290000565f7933412efaf876a
David Hacker <dmhacker@protonmail.com>
2019-12-30 18:06:23 -0800

    Add gitignore to remove build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmhacker/projects/jester
-# Build directory: /home/dmhacker/projects/jester/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/dmhacker/projects/jester/build/tests_include-b858cb2.cmake")
--- build/tests_tests-b858cb2.cmake
+++
@@ -1,1 +0,0 @@
-set( tests_TESTS)
