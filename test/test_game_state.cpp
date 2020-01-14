5c06e4aaf4425455c1e81b3c7ef6cc31f66667ef
David Hacker <dmhacker@protonmail.com>
2020-01-14 09:16:15 -0800

    Add more tests for game state and view

--- test/test_game_state.cpp
+++ test/test_game_state.cpp
@@ -1,17 +1,190 @@
 #include <catch.hpp>
 
-#include "../src/rules/game_state.hpp"
 #include "../src/constants.hpp"
+#include "../src/players/random_player.hpp"
+#include "../src/rules/game_state.hpp"
 
 using namespace jester;
 
-TEST_CASE("Game resets work correctly")
+static std::mt19937 rng;
+
+TEST_CASE("Game is in a reset state")
 {
-    std::mt19937 rng;
-    size_t deck_size = (Constants::instance().MAX_RANK - Constants::instance().MIN_RANK + 1) * 4;
-    for (size_t i = 2; i < 6; i++) {
-        GameState state(i, rng);
-        REQUIRE(state.deck().size() == deck_size - Constants::instance().HAND_SIZE * i);
-        REQUIRE(state.hiddenCards().size() == deck_size - 1);
+    size_t card_count = (Constants::instance().MAX_RANK
+                            - Constants::instance().MIN_RANK + 1)
+        * 4;
+    for (size_t pcnt = 2; pcnt < 6; pcnt++) {
+        GameState state(pcnt, rng);
+        // Check sizes for common methods
+        REQUIRE(state.playerCount() == pcnt);
+        REQUIRE(state.deck().size()
+            == card_count - Constants::instance().HAND_SIZE * pcnt);
+        REQUIRE(state.hiddenCards().size()
+            == card_count - 1);
+        REQUIRE(state.currentPlayerId() == 0);
+        REQUIRE(state.attackerId() == 0);
+        REQUIRE(state.defenderId() == 1);
+        REQUIRE(state.firstMove());
+        REQUIRE(state.attackerNext());
+        REQUIRE(state.currentAttack().empty());
+        REQUIRE(state.currentDefense().empty());
+        REQUIRE(state.winOrder().empty());
+        REQUIRE(state.attackOrder().size() == pcnt);
+        REQUIRE(!state.finished());
+        REQUIRE(state.deck().back() == state.trumpCard());
+        REQUIRE(state.trumpSuit() == state.trumpCard().suit());
+        REQUIRE(state.nextActions().size() == state.hand(0).size());
+        REQUIRE(std::find(state.nextActions().begin(),
+                    state.nextActions().end(),
+                    Action())
+            == state.nextActions().end());
+        // Check to make sure all cards are distributed uniquely
+        std::unordered_set<Card> cards;
+        for (size_t pid = 0; pid < pcnt; pid++) {
+            auto& hand = state.hand(pid);
+            for (auto& card : hand) {
+                REQUIRE(cards.find(card) == cards.end());
+                cards.insert(card);
+            }
+        }
+        for (auto& card : state.deck()) {
+            REQUIRE(cards.find(card) == cards.end());
+            cards.insert(card);
+        }
+        REQUIRE(cards.size() == card_count);
+    }
+}
+
+TEST_CASE("Game is in a defending state")
+{
+    RandomPlayer player;
+    for (size_t pcnt = 2; pcnt < 6; pcnt++) {
+        GameState state(pcnt, rng);
+        // Playing 1 action results in a defender's turn
+        for (size_t a = 0; a < 1; a++) {
+            auto action = player.nextAction(
+                state.currentPlayerView());
+            state.validateAction(action);
+            REQUIRE(std::find(state.nextActions().begin(),
+                        state.nextActions().end(), action)
+                != state.nextActions().end());
+            state.playAction(action);
+        }
+        REQUIRE(state.playerCount() == pcnt);
+        REQUIRE(!state.firstMove());
+        REQUIRE(state.currentPlayerId() == state.defenderId());
+        REQUIRE(state.currentAttack().size() == 1);
+        REQUIRE(state.currentDefense().size() == 0);
+        REQUIRE(!state.attackerNext());
+        REQUIRE(!state.finished());
+        REQUIRE(!state.nextActions().empty());
+        REQUIRE(state.winOrder().empty());
+        REQUIRE(state.attackOrder().size() == pcnt);
+        REQUIRE(!state.deck().empty());
+        REQUIRE(state.deck().back() == state.trumpCard());
+        for (size_t pid = 0; pid < pcnt; pid++) {
+            REQUIRE(std::find(state.attackOrder().begin(),
+                        state.attackOrder().end(), pid)
+                != state.attackOrder().end());
+        }
+    }
+}
+
+TEST_CASE("Game is in an attacking state")
+{
+    RandomPlayer player;
+    for (size_t pcnt = 2; pcnt < 6; pcnt++) {
+        GameState state(pcnt, rng);
+        // Playing 2 actions results in an attacker's turn
+        for (size_t a = 0; a < 2; a++) {
+            auto action = player.nextAction(
+                state.currentPlayerView());
+            state.validateAction(action);
+            REQUIRE(std::find(state.nextActions().begin(),
+                        state.nextActions().end(), action)
+                != state.nextActions().end());
+            state.playAction(action);
+        }
+        REQUIRE(state.playerCount() == pcnt);
+        REQUIRE(!state.firstMove());
+        REQUIRE(state.currentPlayerId() == state.attackerId());
+        REQUIRE(state.currentAttack().size() == state.currentDefense().size());
+        REQUIRE(state.attackerNext());
+        REQUIRE(!state.finished());
+        REQUIRE(!state.nextActions().empty());
+        REQUIRE(state.winOrder().empty());
+        REQUIRE(state.attackOrder().size() == pcnt);
+        REQUIRE(!state.deck().empty());
+        REQUIRE(state.deck().back() == state.trumpCard());
+        for (size_t pid = 0; pid < pcnt; pid++) {
+            REQUIRE(std::find(state.attackOrder().begin(),
+                        state.attackOrder().end(), pid)
+                != state.attackOrder().end());
+        }
+    }
+}
+
+TEST_CASE("Game is in a finished state")
+{
+    RandomPlayer player;
+    for (size_t pcnt = 2; pcnt < 6; pcnt++) {
+        GameState state(pcnt, rng);
+        while (!state.finished()) {
+            // Check to make sure all players are accounted for
+            REQUIRE(state.attackOrder().size() + state.winOrder().size() == pcnt);
+            // Check to make sure that player hands are being replenished
+            if (!state.deck().empty()) {
+                if (state.currentAttack().empty() && state.currentDefense().empty()) {
+                    for (size_t pid = 0; pid < pcnt; pid++) {
+                        REQUIRE(state.hand(pid).size() >= Constants::instance().HAND_SIZE);
+                    }
+                }
+                REQUIRE(state.hand(state.attackerId()).size() 
+                        + state.currentAttack().size() 
+                        >= Constants::instance().HAND_SIZE);
+                REQUIRE(state.hand(state.defenderId()).size() 
+                        + state.currentDefense().size() 
+                        >= Constants::instance().HAND_SIZE);
+            }
+            // Check to make sure current attack and defense have correct sizes
+            if (state.attackerNext()) {
+                REQUIRE(state.currentAttack().size() == state.currentDefense().size());
+            }
+            else {
+                REQUIRE(state.currentAttack().size() == state.currentDefense().size() + 1);
+            }
+            auto action = player.nextAction(
+                state.currentPlayerView());
+            state.validateAction(action);
+            REQUIRE(std::find(state.nextActions().begin(),
+                        state.nextActions().end(), action)
+                != state.nextActions().end());
+            state.playAction(action);
+        }
+        REQUIRE(state.playerCount() == pcnt);
+        REQUIRE(!state.firstMove());
+        REQUIRE(state.attackOrder().empty());
+        REQUIRE(state.nextActions().empty());
+        REQUIRE(state.winOrder().size() == pcnt);
+        REQUIRE(state.deck().empty());
+        for (size_t pid = 0; pid < pcnt; pid++) {
+            REQUIRE(std::find(state.winOrder().begin(),
+                        state.winOrder().end(), pid)
+                != state.winOrder().end());
+        }
+    }
+}
+
+TEST_CASE("Invalid yield is caught by validate")
+{
+    for (size_t pcnt = 2; pcnt < 6; pcnt++) {
+        GameState state(pcnt, rng);
+        // Player should not be able to yield (first attack)
+        try {
+            state.validateAction(Action());
+            REQUIRE(false);
+        } catch (std::logic_error& le) {
+            REQUIRE(!std::string(le.what()).empty());
+        }
     }
 }
--- test/test_game_view.cpp
+++ test/test_game_view.cpp
@@ -1,16 +1,85 @@
 #include <catch.hpp>
 
+#include "../src/constants.hpp"
+#include "../src/players/random_player.hpp"
 #include "../src/rules/game_state.hpp"
 #include "../src/rules/game_view.hpp"
-#include "../src/constants.hpp"
 
 using namespace jester;
 
-TEST_CASE("Game view has correct deck size")
+static std::mt19937 rng;
+
+TEST_CASE("Game view reflects current game state")
 {
-    std::mt19937 rng;
-    for (size_t i = 2; i < 6; i++) {
-        GameState state(i, rng);
-        REQUIRE(state.deck().size() == state.currentPlayerView().deckSize());
+    RandomPlayer player;
+    for (size_t pcnt = 2; pcnt < 6; pcnt++) {
+        GameState state(pcnt, rng);
+        while (!state.finished()) {
+            for (size_t pid = 0; pid < pcnt; pid++) {
+                GameView view(state, pid);
+                REQUIRE(view.playerId() == pid);
+                REQUIRE(view.firstMove() == state.firstMove());
+                REQUIRE(!view.finished());
+                REQUIRE(view.attackerNext() == state.attackerNext());
+                REQUIRE(view.playerCount() == pcnt);
+                REQUIRE(view.deckSize() == state.deck().size());
+                REQUIRE(view.nextActions() == state.nextActions());
+                REQUIRE(view.winOrder() == state.winOrder());
+                REQUIRE(view.attackOrder() == state.attackOrder());
+                REQUIRE(view.hiddenCards() == state.hiddenCards());
+                REQUIRE(view.currentAttack() == state.currentAttack());
+                REQUIRE(view.currentDefense() == state.currentDefense());
+                REQUIRE(view.trumpCard() == state.trumpCard());
+                REQUIRE(view.trumpSuit() == state.trumpSuit());
+                REQUIRE(view.attackerId() == state.attackerId());
+                REQUIRE(view.defenderId() == state.defenderId());
+            }
+            auto action = player.nextAction(
+                state.currentPlayerView());
+            state.validateAction(action);
+            state.playAction(action);
+        }
+    }
+}
+
+TEST_CASE("Hands are hidden correctly")
+{
+    RandomPlayer player;
+    for (size_t pcnt = 2; pcnt < 6; pcnt++) {
+        GameState state(pcnt, rng);
+        while (!state.finished()) {
+            for (size_t pid = 0; pid < pcnt; pid++) {
+                GameView view(state, pid);
+                REQUIRE(view.hand() == state.hand(pid));
+                REQUIRE(view.visibleHand(pid) == state.hand(pid));
+                for (size_t pid2 = 0; pid2 < pcnt; pid2++) {
+                    REQUIRE(view.visibleHand(pid2).size()
+                            + view.hiddenHandSize(pid2)
+                        == state.hand(pid2).size());
+                    REQUIRE(view.handSize(pid2)
+                        == state.hand(pid2).size());
+                }
+                for (size_t pid2 = 0; pid2 < pcnt; pid2++) {
+                    if (pid != pid2) {
+                        size_t hcnt = 0;
+                        for (auto& card : state.hand(pid2)) {
+                            if (state.hiddenCards().find(card) 
+                                    == state.hiddenCards().end()) {
+                                auto vhand = view.visibleHand(pid2);
+                                REQUIRE(vhand.find(card) != vhand.end());
+                            }
+                            else {
+                                hcnt++;
+                            }
+                        }
+                        REQUIRE(view.hiddenHandSize(pid2) == hcnt);
+                    }
+                }
+            }
+            auto action = player.nextAction(
+                state.currentPlayerView());
+            state.validateAction(action);
+            state.playAction(action);
+        }
     }
 }
